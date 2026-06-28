7a5b97d2f243b698196e82fddbc4c54e40046e67
Rijal Rifai <rifairijal97@gmail.com>
2026-06-28 17:57:11 +0700

    fix: remove duplicate lockfile and fix turbopack root config
    - Remove package-lock.json from root to eliminate multiple lockfiles warning
    - Remove archiver dependency from root package.json
    - Update build-extension.js to use archiver from web/node_modules
    - Add turbopack.root config in next.config.ts to fix workspace detection
    - Fixes CPU 100% issue caused by Turbopack monitoring wrong directories

--- build-extension.js
+++ build-extension.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const archiver = require('archiver').default || require('archiver');
+const archiver = require('./web/node_modules/archiver');
 
 const environments = ['local', 'qa', 'prod'];
 
